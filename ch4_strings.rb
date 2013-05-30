286e923f561a6c5275328ecd2e2b4eba348d6968
tcshao <tcshao@gmail.com>
2013-05-30 07:23:01 -0700

    Added string chapter

---
+++ ch4_strings.rb
@@ -0,0 +1,33 @@
+require 'pp'
+
+# single quoted strings offer little interpretation
+string_with_a_quote = 'Say it ain\'t so'
+
+pp string_with_a_quote
+
+# you can embed a backslash with a backslash
+# book example says this should print one backslash, but output is showing 2!?
+string_with_a_backslash = 'This is a backslash: \\ '
+
+string_with_interpolation = "There is another string inside: #{string_with_a_quote}"
+
+pp string_with_interpolation
+
+# the %q just takes anything between the {}'s'
+literal_string = %q{Q-Bert is the best \\@$$!#@$%%@#$"@"$ }
+
+pp literal_string
+
+# chomp cuts off the last trailing white space characters
+
+puts "hello\n\n".chomp
+
+# split creates an array split by the selected carachter
+
+puts "make;this;into;an;array".split(';')
+
+# sub replaces the first occurance
+puts 'first first first first'.sub('first','second')
+
+# gsub replaces all
+puts 'first first first first'.gsub('first','second')
